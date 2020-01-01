0cdafbe916e57d047e0759b78aba2264001cf843
Dolso <omnogom24@yandex.ru>
2020-01-02 02:35:43 +0300

    add class
    методы работы с бд через PDO вывел в отдельный класс

---
+++ PDOdb.php
@@ -0,0 +1,93 @@
+<?php
+
+class PDOdb
+{
+
+    public $host;
+    public $dbname;
+    public $login;
+    public $password;
+
+    function __construct($host, $dbname, $login, $password)
+    {
+        $this->host = $host;
+        $this->dbname = $dbname;
+        $this->login = $login;
+        $this->password = $password;
+    }
+
+    function insert($id, $meadal, $nomer, $pravilotv, $name, $popitki, $active)
+    {
+        try {
+            global $host;
+            global $dbname;
+            global $login;
+            global $password;
+
+            $DBH = new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=UTF8", $login, $password);
+            $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $data = array($id,$meadal,$nomer,$pravilotv,$name,$popitki,$active);
+            $STH = $DBH -> prepare("INSERT INTO tik (id, rank, nomer, pravilotv, name, popitki, active) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $STH -> execute($data);
+        }
+        catch(PDOException $e) {
+            echo "PROBLEVA S INSERT";
+            file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+        }
+    }
+    //функция для запроса UPDATE, если $d true то нужно прибавить к параметру 1
+    function update($parametr, $znach, $chat_id, $d)
+    {
+        try {
+            global $host;
+            global $dbname;
+            global $login;
+            global $password;
+
+            $DBH = new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=UTF8", $login, $password);
+            $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+            if ($d == true) {
+                $stmt = $DBH-> query("SELECT ".$parametr." FROM tik WHERE id =".$chat_id);
+                $stmt-> setFetchMode(PDO::FETCH_ASSOC);
+                $row = $stmt -> fetch();
+                $row[$parametr] += 1;
+                $data = array($row[$parametr],$chat_id);
+                $STH = $DBH -> prepare("UPDATE tik SET ".$parametr." = ? WHERE id = ?");
+                $STH -> execute($data);
+            }
+            else {
+                $data = array($znach,$chat_id);
+                $STH = $DBH -> prepare("UPDATE tik SET ".$parametr." = ? WHERE id = ?");
+                $STH -> execute($data);
+            }
+        }
+        catch(PDOException $e) {
+            echo "PROBLEMA S UPDATE";
+            file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+        }
+    }
+    //функция для запроса SELECT
+    function select($parametr, $chat_id)
+    {
+        try {
+            global $host;
+            global $dbname;
+            global $login;
+            global $password;
+
+            $DBH = new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=UTF8", $login, $password);
+            $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $stmt = $DBH->query("SELECT ".$parametr." FROM tik WHERE id =".$chat_id);
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $row = $stmt->fetch();
+            return $row;
+        }
+        catch(PDOException $e) {
+            echo "PROBLEMA S SELECT";
+            file_put_contents('PDOErrors.txt', $e->getMessage(), FILE_APPEND);
+        }
+    }
+}
+
+?>
